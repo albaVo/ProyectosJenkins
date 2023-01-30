6cf5a588a6f0bf9bf4bab52dfd3ce17fdde4bcaa
DESKTOP-AMN55HB\alba1 <alba12vo@gmail.com>
2023-01-30 13:59:29 +0100

    jenkins

---
+++ pTienda/dslTienda.groovy
@@ -0,0 +1,20 @@
+// import jenkins-auto-approve-script.groovy
+
+job('Job-pTienda') {
+    description('Job Padre para crear una tarea Hija que despliegue la App Tienda del Corte Ingles')
+    scm {
+        git('https://github.com/albaVo/PTienda.git', 'master'){ 
+            node -> 
+                node / gitConfigName('albaVo')
+                node / gitConfigEmail('alba12vo@gmail.com')
+        }
+    }
+    triggers {
+     //   scm('H/5 * * * *')
+        githubPush()
+    }
+    
+    steps {
+        shell("bash install.sh")
+    }
+}
